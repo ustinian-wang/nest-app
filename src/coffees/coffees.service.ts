8ef54da6713b11333a8ff9c6614c389e526ce7e5
jser <872440899@qq.com>
2024-08-24 10:03:33 +0800

    feat: impl coffee controller

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,37 +1,40 @@
-import { Injectable } from "@nestjs/common";
-import { Coffee } from "./entities/coffee.entities";
+import {Injectable} from "@nestjs/common";
+import {Coffee} from "./entities/coffee.entities";
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1, name: "Shipwreck Roast", brand: "Buddy Brew", flavors: ["chocolate", "vanilla"]
+    private coffees: Coffee[] = [{
+        id: 1, name: "Shipwreck Roast", brand: "Buddy Brew", flavors: ["chocolate", "vanilla"]
+    }, {
+        "id": 2, "name": "test", "brand": "Buddy Brew", "flavors": ["chocolate", "vanilla"]
+    }];
+
+    findAll() {
+        return this.coffees;
     }
-  ];
 
-  findAll(){
-    return this.coffees;
-  }
-
-  findOne(id: string){
-    return this.coffees.find(c => c.id === +id);
-  }
-
-  create(createCoffeeDto: any){
-    this.coffees.push(createCoffeeDto);
-  }
-
-  update(id: string, updateCoffeeDto: any){
-    const existingCoffees = this.findOne(id);
-    if(existingCoffees){
-      // update the existing
+    findOne(id: string) {
+        return this.coffees.find(c => c.id === +id);
     }
-  }
 
-  remove(id: string){
-    const coffeeIndex = this.coffees.findIndex(c => c.id === +id);
-    if(coffeeIndex>=0){
-      this.coffees.splice(coffeeIndex, 1)
+    create(createCoffeeDto: any) {
+        this.coffees.push(createCoffeeDto);
     }
-  }
+
+    update(id: string, updateCoffeeDto: any) {
+        const existingCoffees = this.findOne(id);
+        console.log("jser existingCoffees", existingCoffees);
+        if (existingCoffees) {
+            // update the existing
+            Object.assign(existingCoffees, updateCoffeeDto);
+        }
+    }
+
+    remove(id: string) {
+        const coffeeIndex = this.coffees.findIndex(c => c.id === +id);
+        console.log("jser coffeeIndex", coffeeIndex);
+        if (coffeeIndex >= 0) {
+            this.coffees.splice(coffeeIndex, 1)
+        }
+    }
 }
